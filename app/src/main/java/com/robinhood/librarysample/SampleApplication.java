fef32f9912d175b0033df153415b9d865bd82e7e
Sean <haveahopes@gmail.com>
2017-07-24 01:44:02 +0900

    Add Library

--- app/src/main/java/com/robinhood/librarysample/SampleApplication.java
+++ app/src/main/java/com/robinhood/librarysample/SampleApplication.java
@@ -2,7 +2,12 @@ package com.robinhood.librarysample;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.robinhood.librarysample.base.util.SharedPreferencesService;
+import com.squareup.leakcanary.LeakCanary;
+
+import okhttp3.OkHttpClient;
 
 /**
  * Created by sean on 2017. 7. 23..
@@ -12,6 +17,16 @@ public class SampleApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        Stetho.initializeWithDefaults(this);
+        new OkHttpClient.Builder()
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
         SharedPreferencesService.getInstance().load(getApplicationContext());
     }
 }
